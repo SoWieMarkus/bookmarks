5c4b0ec8aad6199bada5589bf030d1da5a990bf6
SoWieMarkus <wie.markus@web.de>
2025-06-05 11:44:05 +0200

    fixed loading text not aligned

--- packages/frontend/src/app/pages/posts-page/posts-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-	selector: "app-posts-page",
-	imports: [],
-	templateUrl: "./posts-page.component.html",
-	styleUrl: "./posts-page.component.scss",
-})
-export class PostsPage {}
